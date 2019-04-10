ba20667db46ebdb2f9fbcc3ee3e5a0a828ecbf91
欧阳晨灵 <ouyangchenling@cangoonline.com>
2019-04-10 14:43:06 +0800

    开启Transation配置

--- src/main/java/com/oycl/demo/common/config/MyBatisConfig.java
+++ src/main/java/com/oycl/demo/common/config/MyBatisConfig.java
@@ -13,10 +13,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableTransactionManagement(proxyTargetClass = true)
 @MapperScan("com.oycl.demo.orm")
 public class MyBatisConfig {
 
